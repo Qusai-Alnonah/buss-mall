6915bd926133985f32e4cbe8ee75a48164513954
Qusai-Alnonah <kusaialnona@yahoo.com>
2021-04-19 18:06:46 +0300

    add chart

--- app.js
+++ app.js
@@ -16,6 +16,7 @@ function Product(name,source){
     this.name =name;
     this.source =source; 
     this.votes = 0 ;
+    this.shown = 0 ;
     Product.allImage.push(this);
 }
 Product.allImage =[];
@@ -96,3 +97,42 @@ function genrateRandomIndex(){
     return Math.floor(Math.random()*Product.allImage.length);
 
 }
+
+
+
+
+let ctx = document.getElementById('myChart')
+let myChart = new Chart(ctx, {
+    type: 'bar',
+    data: {
+        labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+        datasets: [{
+            label: '# of Votes',
+            data: [12, 19, 3, 5, 2, 3],
+            backgroundColor: [
+                'rgba(255, 99, 132, 0.2)',
+                'rgba(54, 162, 235, 0.2)',
+                'rgba(255, 206, 86, 0.2)',
+                'rgba(75, 192, 192, 0.2)',
+                'rgba(153, 102, 255, 0.2)',
+                'rgba(255, 159, 64, 0.2)'
+            ],
+            borderColor: [
+                'rgba(255, 99, 132, 1)',
+                'rgba(54, 162, 235, 1)',
+                'rgba(255, 206, 86, 1)',
+                'rgba(75, 192, 192, 1)',
+                'rgba(153, 102, 255, 1)',
+                'rgba(255, 159, 64, 1)'
+            ],
+            borderWidth: 1
+        }]
+    },
+    options: {
+        scales: {
+            y: {
+                beginAtZero: true
+            }
+        }
+    }
+});
